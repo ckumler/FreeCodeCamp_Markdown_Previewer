2fe447a9b2fa7af461b068a475e8bad453ee6c96
Corey <corey.kumler@gmail.com>
2023-05-18 19:28:06 -0400

    update defaultEditorText

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,7 @@ import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 function App() {
-  const defaultEditorText = `
-  # Welcome to the Markdown Previewer!
-  
-  ## This is a sub-header (H2 size)
-  
-  Here's a link: [My Github](https://github.com/ckumler)
-  
-  Here's some inline code: \`const hello = "Hello, world!";\`
-  
-  Here's a code block:
-  
-  \`\`\`
-  function greet() {
-    const greeting = "Hello, world!";
-    console.log(greeting);
-  }
-  \`\`\`
-  
-  Here's a list:
-  - Item 1
-  - Item 2
-  - Item 3
-  
-  Here's a blockquote:
-  
-  > "Hello, world!" - Traditional greeting for new programmers
-  
-  Here's an image:
-  
-  ![Image](https://via.placeholder.com/150)
-  
-  Here's some **bolded text**.
-  `;
+  const defaultEditorText = "";
 
   const [markdownContent, setMarkdownContent] = useState(defaultEditorText);
 
